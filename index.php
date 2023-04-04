1e476efe489598e33a992afbfaad05cbf8f69a21
Lilian <antonialiliandsv@gmail.com>
2023-04-04 14:41:28 -0300

    adicionando página de indice para navegação no projeto

---
+++ index.php
@@ -0,0 +1,12 @@
+<!DOCTYPE html>
+<html>
+	<head>
+		<title>Índice</title>
+	</head>
+	<body>
+		<h1>Índice</h1>
+		<ul>
+			<li><a href="/Exercicio2/index.html">Exercício 2</a></li>
+		</ul>
+	</body>
+</html>
