522496aaa26048aa16d54a580f763dc4c48cd3c2
Lilian <antonialiliandsv@gmail.com>
2023-04-22 10:52:34 -0300

    Arrumando indentação do código

--- Exercicio1/index.php
+++ Exercicio1/index.php
@@ -5,11 +5,11 @@
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=drive-width, initial-scale=1.0">
     <link rel="stylesheet" href="style.css">
-    <title> Exercicio 1</title>
+    <title> Exercicio 1 </title>
 
   </head>
   <body>
-    <h1>Exercicio 1</h1>
+    <h1>Exercicio 1 </h1>
     <form action="" method="post">
       <div>
         <label form="value1 "> Digite o primeiro número: </label>
@@ -22,24 +22,26 @@
       <input type="submit" value="Enviar">
     </form>
 
-    <?php
+  <?php
+
     if (key_exists('value1', $_POST)){
 
-    $value1 = $_POST["value1"];
-    $value2 = $_POST["value2"];
+      $value1 = $_POST["value1"];
+      $value2 = $_POST["value2"];
 
-    $soma = $value1 + $value2;
-    $resultado = "";
+      $soma = $value1 + $value2;
+      $resultado = "";
 
     if ($soma >= 20) {
-    $resultado = $soma + 8;
-    }else{
-    $resultado = $soma - 5;
+      $resultado = $soma + 8;
+    }
+    else {
+      $resultado = $soma - 5;
     }
      
     echo "<div class='result'> O resultado foi $resultado </div>";
 
-}
+    }
 ?>
   </body>
 </html>
