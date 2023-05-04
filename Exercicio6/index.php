f44f0db0535bce838209ca7509d2a6cffd2b8b69
Lilian <antonialiliandsv@gmail.com>
2023-05-04 12:40:49 -0300

    Adicionando formulario do exercicio6

---
+++ Exercicio6/index.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="UTF-8">
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="viewport" content="width=drive-width, initial-scale=1.0">
+        <link rel="stylesheet" href="style.css">
+        <title>Exercicio 6</title>
+	</head>
+
+<body>
+	<form action="Exercicio6/index.php" method="get">
+		<label> Informe 1 número inteiro </label>
+		<input type="text" id="num" name="num">
+		<input type="submit" value="Enviar">
+	</form>
+</body>
+
+</html>
