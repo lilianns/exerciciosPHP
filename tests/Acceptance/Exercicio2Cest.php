9b6e0d1d0370dbb7bb3503664cac7d99d3f30b83
Lilian <antonialiliandsv@gmail.com>
2023-05-04 15:06:31 -0300

    Adicionando teste automatizado

---
+++ tests/Acceptance/Exercicio2Cest.php
@@ -0,0 +1,45 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exercicio2Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function TitleExercicio2Test(AcceptanceTester $I)
+    {
+     $I->amOnPage("/Exercicio2");
+
+     $I->seeInTitle("Exercicio 2");
+     $I->see('Exercicio 2');
+
+    }
+
+    public function FormExercicio2Test(AcceptanceTester $I)
+    {
+     $I->amOnPage("/Exercicio2");
+
+     $I->fillField("Divisor", "5");
+     $I->click('Enviar');
+     //$I->see('É divisível por 5')
+
+     $I->fillField("Divisor", "7");
+     $I->click('Enviar');
+     //$I->see('Não é divisível por 10, 5 e nem por 2.')
+
+     $I->fillField("Divisor", "10");
+     $I->click('Enviar');
+     //$I->see('É divisível por 10')
+
+     $I->fillField("Divisor", "12");
+     $I->click('Enviar');
+     //$I->see('É divisível por 2')
+
+    }
+}
