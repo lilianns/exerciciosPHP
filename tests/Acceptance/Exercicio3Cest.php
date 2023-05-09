7f58ded060069d19a64fb8144e402071c286d3b5
Lilian <antonialiliandsv@gmail.com>
2023-05-09 13:42:48 -0300

    Adicionando teste automatizado do back

---
+++ tests/Acceptance/Exercicio3Cest.php
@@ -0,0 +1,58 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exercicio3Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function FromExercicio3Test(AcceptanceTester $I)
+    {
+    $I->amOnPage("/Exercicio3");
+
+    $I->fillField("nome", "maria");
+    $I->fillField("idade", "24");
+    $I->fillField("feminino", "masculino"); //teste do sexo
+    $I->click('Enviar');
+
+    $I->see(" --- NAO ACEITA");
+
+
+    }
+
+    public function FromExercicio3AceitaTest(AcceptanceTester $I)
+    {
+    $I->amOnPage("/Exercicio3");
+
+    
+    $I->fillField("nome", "maria");
+    $I->fillField("idade", "24");
+    $I->fillField("feminino", "feminino");
+    $I->click('Enviar');
+
+    $I->see(" --- ACEITA");
+
+
+    }
+
+    public function FromExercicio3IdadeTest(AcceptanceTester $I)
+    {
+    $I->amOnPage("/Exercicio3");
+        
+    $I->fillField("nome", "maria");
+    $I->fillField("idade", "26");  //teste de idade
+    $I->fillField("feminino", "feminino");
+    $I->click('Enviar');
+
+    $I->see(" --- NAO ACEITA");
+
+
+    }
+    
+}
