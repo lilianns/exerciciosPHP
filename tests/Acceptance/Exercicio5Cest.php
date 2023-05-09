2c478d1cf803c546e4bcfa18a92e13205044886f
Lilian <antonialiliandsv@gmail.com>
2023-05-09 15:30:01 -0300

    Adicionando teste automatizado

--- tests/Acceptance/Exercicio5Cest.php
+++ tests/Acceptance/Exercicio5Cest.php
@@ -14,12 +14,33 @@ class Exercicio5Cest
     // tests
     public function Exercicio5Test(AcceptanceTester $I)
     {
-        $I->amOnPage("/Exercicio5");
+    
+    $I->amOnPage('/Exercicio5');
 
-        $I->FillField("num1", "2");
-        $I->fillField("num2", "2");
-        $I->fillField("num3", "5");
-        $I->click("Enviar");
+    $I->fillField("num1", "3");
+    $I->fillField("num2", "4");
+    $I->fillField("num3", "5");
+    $I->click('Enviar');
+    $I->see("Triângulo escaleno");
+
+    $I->fillField("num1", "3");
+    $I->fillField("num2", "3");
+    $I->fillField("num3", "3");
+    $I->click('Enviar');
+    $I->see("Triângulo equilátero");
+
+    $I->fillField("num1", "3");
+    $I->fillField("num2", "3");
+    $I->fillField("num3", "2");
+    $I->click('Enviar');
+    $I->see("Triângulo isósceles");
+
+    $I->fillField("num1", "3");
+    $I->fillField("num2", "2");
+    $I->fillField("num3", "8");
+    $I->click('Enviar');
+    $I->see("Os valores informados não formam um triângulo");
+
 
     }
 }
