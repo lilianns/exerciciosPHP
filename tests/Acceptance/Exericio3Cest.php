1600db8bea79fca30392994e7c63a802fc809c3d
Lilian <antonialiliandsv@gmail.com>
2023-05-09 13:22:59 -0300

    Adicionando teste automatizado do front

---
+++ tests/Acceptance/Exericio3Cest.php
@@ -0,0 +1,25 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exericio3Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function FromExercicio3Test(AcceptanceTester $I)
+    {
+    $I->amOnPage("/Exercicio3");
+
+     $I->fillField("nome", "maria");
+     $I->fillField("idade", "25");
+     $I->fillField("feminino", "masculino");
+     $I->click('Enviar');
+     
+    }
+}
