273b5738322b401603c8fe3c1f1ee15c654d36ce
Lilian <antonialiliandsv@gmail.com>
2023-05-22 13:17:27 -0300

    Arrumando indentação, codigo e espaços

--- Exercicio4/index.php
+++ Exercicio4/index.php
@@ -7,41 +7,46 @@
     <link rel="stylesheet" href="style.css">
 	<title> Exercicio4 </title>
 </head>
-
 <body>
 	<form action="" method="post">
         <h2>Insira três números:</h2>
-
         <input type="text" name="numero1" id="numero1">
-    </br>
-        <input type="text" name = "numero2" id="numero2">
-    </br>
-        <input type="text" name = "numero3" id="numero3">
-    </br>
+        <input type="text" name="numero2" id="numero2">
+        <input type="text" name="numero3" id="numero3">
         <input type="submit" value="Enviar">
     </form>
 
 <?php
 
-    if (key_exists('numero1', $_POST)){
+    if (array_key_exists('numero1', $_POST)){
         $numero1 = $_POST['numero1'];
         $numero2 = $_POST['numero2'];
         $numero3 = $_POST['numero3'];
 
-        $numeros = [$numero1, $numero2, $numero3];
-
-        // ordena os números em ordem decrescente
-        rsort($numeros);
-
-        // imprime os números em ordem decrescente
-        echo "<h2> Números em ordem decrescente:</h2>";
-        
-        foreach ($numeros as $numero) {
-            echo "<p class='ordem'>".$numero."<p>";
+        if ($numero1 < $numero2) {
+            $aux = $numero1;
+            $numero1 = $numero2;
+            $numero2 = $aux;
         }
+        
+        if ($numero1 < $numero3) {
+            $aux = $numero1;
+            $numero1 = $numero3;
+            $numero3 = $aux;
+        }
+        
+        if ($numero2 < $numero3) {
+            $aux = $numero2;
+            $numero2 = $numero3;
+            $numero3 = $aux;
+        }
+
+        echo "<h2> Números em ordem decrescente:</h2>";
+        echo "<p class='ordem'>" . $numero1 . "<p>";
+        echo "<p class='ordem'>" . $numero2 . "<p>";
+        echo "<p class='ordem'>" . $numero3 . "<p>";
     }
+
 ?>
-
 </body>
-
 </html>
