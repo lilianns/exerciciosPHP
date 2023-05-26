6e48142879f9d2eea7252406f3ab3e96515225e3
Lilian <antonialiliandsv@gmail.com>
2023-05-26 09:35:44 -0300

    Arrumando fomrulario

---
+++ Exercicio8/index.php
@@ -0,0 +1,18 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=drive-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title> Exercicio 8 </title>
+
+</head>
+<body>
+    <form action="" method="get">
+        <label for="numero"> Digite um número:  </label>
+        <input type="number" id="numero" name="numero">
+        <button type="submit">Enviar</button>
+    </form>
+</body>
+</html>
