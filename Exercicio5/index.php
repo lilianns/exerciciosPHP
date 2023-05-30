832f756b3724e8d33446027f656216f02254c64b
Lilian <antonialiliandsv@gmail.com>
2023-05-30 13:22:23 -0300

    Arrumando indentação

--- Exercicio5/index.php
+++ Exercicio5/index.php
@@ -6,16 +6,16 @@
         <meta name="viewport" content="width=drive-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
         <title> Exercicio 5 </title>
-	</head>
+    </head>
 
 <body>
-	<form action="Exercicio5/index.php" method="get">
-		<label> Informe 3 valores para classificação do triângulo </label>
-		<input type="text" id="num1" name="num1">
+    <form action="" method="get">
+        <label> Informe 3 valores para classificação do triângulo </label>
+        <input type="text" id="num1" name="num1">
         <input type="text" id="num2" name="num2">
         <input type="text" id="num3" name="num3">
-		<input type="submit" value="Enviar">
-	</form>
+        <input type="submit" value="Enviar">
+    </form>
 </body>
 
 </html>
