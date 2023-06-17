fcc28eb0f95964247702868b8b66bd4189ac6bd2
Lilian <antonialiliandsv@gmail.com>
2023-06-17 14:19:09 -0300

    Adicionando teste

---
+++ tests/Acceptance/Exercicio10Cest.php
@@ -0,0 +1,27 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exericio10Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function FromExercicioTest(AcceptanceTester $I)
+    {
+
+        $I->amOnPage("/Exercicio10");
+
+        $I->fillField("numero", "10, 20, -1, -2, -6, -8, -1, -2, -6, -8, -1, -2, -6, -8, -1, -2, -6, -8, -1, -2");
+        $I->click('Enviar');
+
+        $I->see(" Os números que você digitou: 10, 20, -1, -2, -6, -8, -1, -2, -6, -8, -1, -2, -6, -8, -1, -2, -6, -8, -1, -2");
+        $I->see("A soma dos números positivos é: 30");
+        $I->see("O total de números negativos é: 18");
+    }
+}
