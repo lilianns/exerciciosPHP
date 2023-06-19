dd665c87fb129865076d73107e8ce84c465c0a2b
Lilian <antonialiliandsv@gmail.com>
2023-06-19 20:09:47 -0300

    Tentando arrumar conflitos

--- Exercicio5/index.php
+++ Exercicio5/index.php
@@ -6,7 +6,7 @@
         <meta name="viewport" content="width=drive-width, initial-scale=1.0">
         <link rel="stylesheet" href="style.css">
         <title> Exercicio 5 </title>
-    </head>
+	</head>
 
 <body>
     <form action="" method="get">
@@ -14,8 +14,30 @@
         <input type="text" id="num1" name="num1">
         <input type="text" id="num2" name="num2">
         <input type="text" id="num3" name="num3">
-        <input type="submit" value="Enviar">
-    </form>
+		<input type="submit" value="Enviar">
+
+        <?php
+    if(isset($_GET['num1']) && isset($_GET['num2']) && isset($_GET['num3'])) {
+        $num1 = $_GET['num1'];
+        $num2 = $_GET['num2'];
+        $num3 = $_GET['num3'];
+    } else {
+		$num1 = 0;
+		$num2 = 0;
+		$num3 = 0;
+	}
+	    if (($num1 + $num2 > $num3) && ($num1 + $num3 > $num2) && ($num2 + $num3 > $num1)) {
+	      if (($num1 == $num2) && ($num2 == $num3)) {
+	        echo  '<p>Triângulo equilátero</p>';
+	      } else if (($num1 == $num2) || ($num1 == $num3) || ($num2 == $num3)) {
+	        echo '<p>Triângulo isósceles</p>';
+	      } else {
+	        echo '<p>Triângulo escaleno</p>';
+	      }
+	    } else {
+            echo '<p>Os valores informados não formam um triângulo</p>';
+	    }
+?>
 </body>
 
 </html>
