8082aa3b7c8ca7f43963546b498b585acfff18d4
Lilian <antonialiliandsv@gmail.com>
2023-06-20 20:30:43 -0300

    Adicionando inserção

--- Exercicio10/index.php
+++ Exercicio10/index.php
@@ -1,11 +1,14 @@
 <link rel="stylesheet" href="style.css">
 
 <?php
+    include("conexao.php");
+    
     if (isset($_GET['numeros'])) {
         $numeros = explode(',', $_GET['numeros']);
         $somaPositivos = 0;
         $totalNegativos = 0;
         $count = count($numeros);
+        $num = $_GET['numeros'];
 
         for ($i = 0; $i < $count; $i++) {
             $numero = $numeros[$i];
@@ -17,10 +20,21 @@
             }
         }
 
+        $sql="INSERT INTO exercicio10(numeros, somaPositivos, totalNegativos) 
+            VALUES ('$num', $somaPositivos, $totalNegativos)";
+            if(mysqli_query($conexao, $sql)){
+                echo " Cadastrado com sucesso";
+            }
+            else{
+                echo "erro".mysqli_connect_errno($conexao);
+            }
+
         echo '<p class="resultado"> Os números que você digitou: ' . implode(", ", $numeros) . '</p>';
         echo '<p class="resultado"> A soma dos números positivos é: ' . $somaPositivos . '</p>';
         echo '<p class="resultado"> O total de números negativos é: ' . $totalNegativos . '</p>';
 
+        mysqli_close($conexao);
+    
 
         // Botão para voltar ao formulário
         echo '<form action="form.html" method="get">
