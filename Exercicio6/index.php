ccd28b46b072d90eb52a8716b5537f17bbd00fab
Lilian <antonialiliandsv@gmail.com>
2023-06-21 13:23:23 -0300

    Arrumando

--- Exercicio6/index.php
+++ Exercicio6/index.php
@@ -1,50 +1,45 @@
 <!DOCTYPE html>
 <html lang="en">
-    <head>
-        <meta charset="UTF-8">
-        <meta http-equiv="X-UA-Compatible" content="IE=edge">
-        <meta name="viewport" content="width=drive-width, initial-scale=1.0">
-        <link rel="stylesheet" href="style.css">
-        <title>Exercicio 6</title>
-	</head>
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="style.css">
+    <title>Exercicio 6</title>
+</head>
 
 <body>
-	<form action="" method="get">
-		<label> Informe 1 número inteiro entre 1 e 12 para ser exibido o mês correspondente</label>
-		<input type="text" id="num" name="num">
-		<input type="submit" value="Enviar">
-	</form>
-</body>
+    <form action="" method="get">
+        <label>Informe 1 número inteiro entre 1 e 12 para ser exibido o mês correspondente:</label>
+        <input type="text" id="num" name="num">
+        <input type="submit" value="Enviar">
+    </form>
 
 <?php
+    if (isset($_GET['num'])) {
+        $num = $_GET['num'];
+        
+        $meses = [
+            1 => 'janeiro',
+            2 => 'fevereiro',
+            3 => 'março',
+            4 => 'abril',
+            5 => 'maio',
+            6 => 'junho',
+            7 => 'julho',
+            8 => 'agosto',
+            9 => 'setembro',
+            10 => 'outubro',
+            11 => 'novembro',
+            12 => 'dezembro'
+        ];
 
-    if (key_exists('num', $_GET)){
-    $num = $_GET['num'];
+        if ($num >= 1 && $num <= 12) {
+            echo "<div class='result'>O número $num corresponde ao mês de " . $meses[$num] . ".</div>";
+        } else {
+            echo "<div class='result'>Não existe mês correspondente para o número informado.</div>";
+        }
     }
-    
-    $meses = array(
-        1 => 'janeiro',
-        2 => 'fevereiro',
-        3 => 'março',
-        4 => 'abril',
-        5 => 'maio',
-        6 => 'junho',
-        7 => 'julho',
-        8 => 'agosto',
-        9 => 'setembro',
-        10 => 'outubro',
-        11 => 'novembro',
-        12 => 'dezembro'
-    );
-
-    if ($num >= 1 && $num <= 12) {
-        echo "<div class='result'>O número $num corresponde ao mês de " . $meses[$num] . ".</div>";
-    } else {
-        echo "<div class='result'>Não existe mês com esse número.</div>";
-    }
-    
-
-?>
-
-
+    ?>
+</body>
 </html>
