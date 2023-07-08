a40cdfb6258df7263afb2197e9f1ff33951c923f
Lilian <antonialiliandsv@gmail.com>
2023-07-08 13:50:20 -0300

    arrumando teste automatizado

--- tests/Acceptance/Exercicio9Cest.php
+++ tests/Acceptance/Exercicio9Cest.php
@@ -15,18 +15,18 @@ class Exercicio9Cest
     public function Exercicio9Test(AcceptanceTester $I)
     {
     
-        $I->amOnPage('/Exercicio9');
-
-        $I->fillField("countRepeat", "3");
+        $I->amOnPage('/Exercicio9/form.html');
+        $I->fillField('countRepeat', '3');
         $I->click('Enviar');
         $I->see("SOL");
         $I->see("SOL");
         $I->see("SOL");
 
-        $I->fillField("countRepeat", "abc");
+        $I->amOnPage('/Exercicio9/form.html');
+        $I->fillField('countRepeat', 'abc');
         $I->click('Enviar');
         $I->see("Por favor, insira um número válido.");
-        
+
         
     }
 }
