b4c82a6f2b97a66e19c8a2b49ae828a439caa4ea
Lilian <antonialiliandsv@gmail.com>
2023-07-10 18:00:22 -0300

    adicionando backend

---
+++ Exercicio13/index.php
@@ -0,0 +1,34 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
+    <div class="section container">
+            <div class="row card-panel">
+<?php
+
+$contador = 0;
+
+if (isset($_POST["number"])) {
+    $number = $_POST["number"];
+    $numbers = explode(',', $number);
+    $count = count($numbers);
+    for ($i = 0; $i < $count; $i++) {
+        $number = ($numbers[$i]);
+            if ($number >= 100 && $number <= 200) {
+                $contador++;
+            } else if ($number == 0  || $number == '') {
+                echo "<h3>Por favor, insira um número válido.</h3>";
+                echo '<form action="form.html" method="post">
+                <button class="btn waves-effect waves-light" type="submit" name="action">Voltar</button>
+                </form>';
+                die();
+            }
+        }
+}
+
+if ($count >0) {
+    echo "<h3>Foi digitado $contador número(s) entre 100 e 200.</h3>";
+    echo '<form action="form.html" method="post">
+    <button class="btn waves-effect waves-light" type="submit" name="action">Voltar</button>
+    </form>';
+}
+
+
+?>
