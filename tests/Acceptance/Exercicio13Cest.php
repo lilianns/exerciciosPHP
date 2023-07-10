9cce3f241bb56b79bb875c97fe31fd17a4061ece
Lilian <antonialiliandsv@gmail.com>
2023-07-10 18:40:12 -0300

    Adicionando testes

---
+++ tests/Acceptance/Exercicio13Cest.php
@@ -0,0 +1,31 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exercicio13Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function Exercicio13Test(AcceptanceTester $I)
+    {
+    
+        $I->amOnPage('/Exercicio13/form.html');
+        $I->fillField('number', '150,165');
+        $I->click('Enviar');
+        $I->see("Foi digitado 2 número(s) entre 100 e 200.");
+
+
+        $I->amOnPage('/Exercicio13/form.html');
+        $I->fillField('number', '');
+        $I->click('Enviar');
+        $I->see("Por favor, insira um número válido.");
+
+        
+    }
+}
