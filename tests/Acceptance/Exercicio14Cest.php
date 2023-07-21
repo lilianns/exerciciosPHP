6b810634ba91b3dec7abe9f04a4ee26d546c9a21
Lilian <antonialiliandsv@gmail.com>
2023-07-21 15:01:24 -0300

    Adicionando teste e back

---
+++ tests/Acceptance/Exercicio14Cest.php
@@ -0,0 +1,28 @@
+<?php
+
+
+namespace Tests\Acceptance;
+
+use Tests\Support\AcceptanceTester;
+
+class Exericio14Cest
+{
+    public function _before(AcceptanceTester $I)
+    {
+    }
+
+    // tests
+    public function FromExercicioTest(AcceptanceTester $I)
+    {
+
+        $I->amOnPage("/Exercicio14/form.html");
+
+        $I->fillField("altura_chico", "150");
+        $I->fillField("crescimento_chico", "2");
+        $I->fillField("altura_juca", "110");
+        $I->fillField("crescimento_juca", "3");
+        $I->click('Calcular');
+
+        $I->see("Serão necessários 41 anos para que Juca seja maior do que Chico.");
+    }
+}
