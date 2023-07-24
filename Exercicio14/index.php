46ca5393f0e050f420ebb49eff0f09b848ca630f
Lilian <antonialiliandsv@gmail.com>
2023-07-24 16:57:23 -0300

    Adicionando banco de dados

--- Exercicio14/index.php
+++ Exercicio14/index.php
@@ -1,3 +1,4 @@
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/materialize/1.0.0/css/materialize.min.css">
 <?php
 
 if (isset($_POST["altura_chico"]) && isset($_POST["altura_juca"]) && isset($_POST["crescimento_chico"]) && isset($_POST["crescimento_juca"])) {
@@ -18,6 +19,21 @@ if (isset($_POST["altura_chico"]) && isset($_POST["altura_juca"]) && isset($_POS
 
     $anos = calculo($altura_chico, $altura_juca, $crescimento_chico, $crescimento_juca);
 
-    echo "Serão necessários $anos anos para que Juca seja maior do que Chico.";
+    echo '<div class="container">';
+    echo '<p class="flow-text"> Serão necessários ' . $anos . ' anos para que Juca seja maior do que Chico.</p>';
+
+    // Botão para voltar ao formulário
+    echo '<form action="form.html" method="get">
+        <input type="submit" value="Voltar" class="btn">
+    </form>';
+    echo '</div>';
+
+    include("conexao.php");
+    echo '</br>';
+
+    $sql="INSERT INTO exercicio14(altura_chico, altura_juca, crescimento_chico, crescimento_juca, anos) 
+        VALUES ($altura_chico, $altura_juca, $crescimento_chico, $crescimento_juca, $anos)";
+
+    mysqli_close($conexao);
 }
 ?>
