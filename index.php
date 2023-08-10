33ac86a9476bd122128fbff46b737c7eddeaec9e
Lilian <antonialiliandsv@gmail.com>
2023-08-10 19:19:21 -0300

    Adicionando o link dos exercicios no indice e estilizando

--- index.php
+++ index.php
@@ -1,25 +1,73 @@
 <!DOCTYPE html>
 <html>
-	<head>
-		<title>Índice</title>
-	</head>
-	<body>
-		<h1>Índice</h1>
-		<ul>
-			<li><a href="/Exercicio1/">Exercício 1</a></li>
-			<li><a href="/Exercicio2/">Exercício 2</a></li>
-			<li><a href="/Exercicio3/">Exercício 3</a></li>
-			<li><a href="/Exercicio4/">Exercício 4</a></li>
-			<li><a href="/Exercicio5/">Exercício 5</a></li>
-			<li><a href="/Exercicio6/">Exercício 6</a></li>
-			<li><a href="/Exercicio7/">Exercício 7</a></li>
-			<li><a href="/Exercicio8/">Exercício 8</a></li>
-			<li><a href="/Exercicio9/form.html">Exercício 9</a></li>
-			<li><a href="/Exercicio10/form.html">Exercício 10</a></li>
-			<li><a href="/Exercicio11/form.php">Exercício 11</a></li>
-			<li><a href="/Exercicio12/form.html">Exercício 12</a></li>
-			<li><a href="/Exercicio13/form.html">Exercício 13</a></li>
+    <head>
+        <title>Índice</title>
+    <style>
+        body {
+        padding: 20px;
+        max-width: 400px;
+        margin: 0 auto;
+        display: flex;
+        flex-wrap: wrap;
+        background-image: linear-gradient(to bottom, #d3545e, #000407);
+        margin-bottom: 20px;
+        }
 
-		</ul>
-	</body>
+        .index-container {
+            background-color: #fff;
+            border-radius: 19px;
+            box-shadow: 0px 0px 10px rgba(0, 0, 0, 0.1);
+            margin: 0 auto;
+            padding: 20px;
+        max-width: 400px;
+        }
+
+        h1 {
+            text-align: center;
+            color: #d3545e;
+            font-size: 50px;
+        }
+        ul {
+            list-style: none;
+            padding: 0;
+            margin: 0;
+            text-align: center;
+        }
+
+        li {
+            margin: 10px ;
+        }
+
+        a {
+            text-decoration: none;
+            color: #333 ;
+            font-size: 30px;
+        }
+    </style>
+    </head>
+    <body>
+        <div class="index-container">
+        <h1>Índice</h1>
+        <ul>
+            <li ><a href="/Exercicio1/">Exercício 1</a></li>
+            <li><a href="/Exercicio2/">Exercício 2</a></li>
+            <li><a href="/Exercicio3/">Exercício 3</a></li>
+            <li><a href="/Exercicio4/">Exercício 4</a></li>
+            <li><a href="/Exercicio5/">Exercício 5</a></li>
+            <li><a href="/Exercicio6/">Exercício 6</a></li>
+            <li><a href="/Exercicio7/">Exercício 7</a></li>
+            <li><a href="/Exercicio8/">Exercício 8</a></li>
+            <li><a href="/Exercicio9/form.html">Exercício 9</a></li>
+            <li><a href="/Exercicio10/form.html">Exercício 10</a></li>
+            <li><a href="/Exercicio11/form.php">Exercício 11</a></li>
+            <li><a href="/Exercicio12/form.html">Exercício 12</a></li>
+            <li><a href="/Exercicio13/form.html">Exercício 13</a></li>
+            <li><a href="/Exercicio14/form.html">Exercício 14</a></li>
+            <li><a href="/Exercicio15/form.html">Exercício 15</a></li>
+            <li><a href="/Exercicio16/form.html">Exercício 16</a></li>
+            <li><a href="/Exercicio17/form.html">Exercício 17</a></li>
+        </ul>
+        </div>
+        
+    </body>
 </html>
