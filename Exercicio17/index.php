f16dcf2fb63c705beda139821be2291c001a0709
Lilian <antonialiliandsv@gmail.com>
2023-08-10 18:11:54 -0300

    Exercicio17: Adicionando o aquivo com ajustes na organizacao do codigo

---
+++ Exercicio17/index.php
@@ -0,0 +1,52 @@
+<?php
+include("conexao.php");
+
+if (isset($_POST["vetor1"])) {
+    $vetor = $_POST["vetor1"];
+
+    $maior = $vetor[0];
+    $menor = $vetor[0];
+    $totalPares = 0;
+    $somaElementos = 0;
+
+    $count = count($vetor);
+
+    for ($i = 0; $i < $count; $i++) {
+        $elemento = $vetor[$i];
+
+        if ($elemento > $maior) {
+            $maior = $elemento;
+        }
+
+        if ($elemento < $menor) {
+            $menor = $elemento;
+        }
+
+        if ($elemento % 2 == 0) {
+            $totalPares++;
+        }
+
+        $somaElementos += $elemento;
+    }
+
+    $percentualPares = ($totalPares / count($vetor)) * 100;
+    $media = $somaElementos / count($vetor);
+}
+
+    $resultado = "Maior valor: " . $maior . "<br>" .
+                 "Menor valor: " . $menor . "<br>" .
+                 "Percentual de números pares: " . $percentualPares . "%<br>" .
+                 "Média dos elementos: " . $media . "<br>";
+
+    echo $resultado;
+
+
+    $vetorStr = implode(', ', $vetor);
+
+    $sql = "INSERT INTO exercicio17(vetor1, maior, menor, media, pares) 
+    VALUES ('$vetorStr', $maior, $menor, $media, $percentualPares)"; 
+    mysqli_query($conexao, $sql);
+
+    mysqli_close($conexao); 
+
+?>
