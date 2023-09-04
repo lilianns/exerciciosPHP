eb1222633feea8016305a0722d9c02b92787c4f1
Lilian <antonialiliandsv@gmail.com>
2023-09-04 14:50:55 -0300

    Adicinando ao Exercicio13 correcao do teste

--- tests/Acceptance/Exercicio12Cest.php
+++ tests/Acceptance/Exercicio12Cest.php
@@ -16,18 +16,16 @@ class Exericio12Cest
     {
 
         $I->amOnPage("/Exercicio12/form.html");
-        $I->fillField('palavra', 'Teste');
+        $I->fillField("palavra", 'Teste');
         $I->click('Imprimir');
+
         $I->see('Teste');
         $I->see('Teste Teste'); 
         $I->see('Teste Teste Teste'); 
         $I->see('Teste Teste Teste Teste'); 
 
-        $I->amOnPage("/Exercicio12/form.html");
-        $palavras = ['Teste'];
-        for ($i = 0; $i < count($palavras); $i++) {
-            $palavra = $palavras[$i];
-            $I->seeInDatabase('exercicio12', ['palavra' => 'Teste']);
-        }
+        $I->seeInDatabase("exercicio12", ["palavras" => "Teste"]);
+        $I->seeInDatabase("exercicio12", ["resultado" => "Teste"]);
+
     }
 }
