0852f4f4f1fc06ea3341b92f9a81cdce53b7e49a
Lilian <antonialiliandsv@gmail.com>
2023-09-25 16:05:59 -0300

    Arrumando a insecao  no banco de dados exercicio 15 e testes automatizados

--- tests/Acceptance/Exercicio15Cest.php
+++ tests/Acceptance/Exercicio15Cest.php
@@ -17,11 +17,12 @@ class Exericio15Cest
 
         $I->amOnPage("/Exercicio15/form.html");
 
-        $I->fillField("massaInicial", "9.0");
+        $I->fillField("massaInicial", "9");
         $I->fillField("massaLimite", "0.9");
         $I->click('Calcular');
-        $I->see("Tempo necessário: 270 segundos");
+        $I->see("270");
 
-        $I->seeInDatabase('exercicio15', ['massaInicial' => '9', 'massaLimite' => '0.9', 'tempo' => '270']);
+        $I->seeInDatabase('exercicio15', ['massaInicial' => '9']);
+        $I->seeInDatabase('exercicio15', ['tempo' => '270']);
     }
 }
